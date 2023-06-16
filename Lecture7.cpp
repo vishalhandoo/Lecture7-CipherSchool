698d70fc5d9fbd94a4c244a7c0a7878426d71379
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-16 12:55:30 +0530

    Add files via upload

---
+++ Lecture7.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int count=0;
+    // while(1){
+    //     cout<<"Chocalates ";
+    //     count++;
+
+    //     if(count>100){
+    //         break;
+    //     }
+    // }
+
+    for(int i = 1 ; ; i++){
+        cout<<i<<" ";
+        if(i>99){
+            break;
+        }
+    }
+
+    return 0;
+}
